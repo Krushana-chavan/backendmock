94187c6a163448395be2f96d0fcaa3c3afd81d6a
Krushana-chavan <104185649+Krushana-chavan@users.noreply.github.com>
2023-01-19 18:01:02 +0530

    done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 app.post("/signup", (req, res) => {
   const { email, password } = req.body;
 
